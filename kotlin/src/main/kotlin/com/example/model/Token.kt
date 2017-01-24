5e83a660f86563c9fec3b4c06339175d751f551b
Ravi Kora <ravi.kora@airisdata.com>
2017-01-24 11:26:58 -0500

    Adding new files - set1

---
+++ kotlin/src/main/kotlin/com/example/model/Token.kt
@@ -0,0 +1,13 @@
+package com.example.model
+
+import java.util.*
+
+/**
+ * A simple class representing a purchase order.
+ * @param orderNumber the purchase order's id number.
+ * @param expiryDate the requested deliveryDate.
+ * @param issueDate the delivery address.
+ */
+data class Token(val expiryDate: Date,
+                 val issueDate: Date
+                 )
